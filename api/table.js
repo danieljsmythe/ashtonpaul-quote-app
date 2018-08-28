610567bfb9e14ca696dfd7fc09664abc77dd56de
danieljsmythe <danieljsmythe@hotmail.co.uk>
2018-08-28 09:11:12 +0100

    Create table.js

---
+++ api/table.js
@@ -0,0 +1,74 @@
+export default table = {
+    measured: {
+      small: {
+        hours: 1,
+        price: 75,
+      },
+      medium: {
+        hours: 2,
+        price: 150,
+      },
+      large: {
+        hours: 3,
+        price: 225,
+      },
+      xLarge: {
+        hours: 4,
+        price: 300,
+      },
+      xxLarge: {
+        hours: 5,
+        price: 675,
+      },
+    },
+
+    level: {
+        small: {
+            price: 600,
+        },
+        medium: {
+            price: 900,
+        },
+        large: {
+            price: 1200,
+        },
+    },
+
+    planAndElevations: {
+        small: {
+            hours: 6,
+            price: 450,
+        },
+        medium: {
+            hours: 8,
+            price: 600,
+        },
+        large: {
+            hours: 10,
+            price: 750,
+        },
+        xLarge: {
+            hours: 12,
+            price: 900,
+        },
+    },
+
+    proposedPlans: {
+        small: {
+            hours: 6,
+            price: 450,
+        },
+        medium: {
+            hours: 8,
+            price: 600,
+        },
+        large: {
+            hours: 10,
+            price: 750,
+        },
+        xLarge: {
+            hours: 12,
+            price: 900,
+        },
+    },
+}
